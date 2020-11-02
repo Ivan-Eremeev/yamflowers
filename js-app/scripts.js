5e153657ac0f0e0debdead44611122cefcfea99a
Ivan <ivan.frontcoder@gmail.com>
2020-11-02 20:04:46 +0500

    edit

--- js-app/scripts.js
+++ js-app/scripts.js
@@ -240,43 +240,44 @@ $(document).ready(function () {
 			buttonImage: '../img/arrow.svg'
 		});	
 		$.datepicker.setDefaults( $.datepicker.regional[ "ru" ] );
-	}
-	// Российская локализация календаря
-	( function( factory ) {
-		if ( typeof define === "function" && define.amd ) {
 
-			// AMD. Register as an anonymous module.
-			define( [ "../widgets/datepicker" ], factory );
-		} else {
+		// Российская локализация календаря
+		( function( factory ) {
+			if ( typeof define === "function" && define.amd ) {
 
-			// Browser globals
-			factory( jQuery.datepicker );
-		}
-	}( function( datepicker ) {
+				// AMD. Register as an anonymous module.
+				define( [ "../widgets/datepicker" ], factory );
+			} else {
 
-	datepicker.regional.ru = {
-		closeText: "Закрыть",
-		prevText: "&#x3C;Пред",
-		nextText: "След&#x3E;",
-		currentText: "Сегодня",
-		monthNames: [ "Январь","Февраль","Март","Апрель","Май","Июнь",
-		"Июль","Август","Сентябрь","Октябрь","Ноябрь","Декабрь" ],
-		monthNamesShort: [ "Янв","Фев","Мар","Апр","Май","Июн",
-		"Июл","Авг","Сен","Окт","Ноя","Дек" ],
-		dayNames: [ "воскресенье","понедельник","вторник","среда","четверг","пятница","суббота" ],
-		dayNamesShort: [ "вск","пнд","втр","срд","чтв","птн","сбт" ],
-		dayNamesMin: [ "Вс","Пн","Вт","Ср","Чт","Пт","Сб" ],
-		weekHeader: "Нед",
-		dateFormat: "dd.mm.yy",
-		firstDay: 1,
-		isRTL: false,
-		showMonthAfterYear: false,
-		yearSuffix: "" };
-	datepicker.setDefaults( datepicker.regional.ru );
+				// Browser globals
+				factory( jQuery.datepicker );
+			}
+		}( function( datepicker ) {
 
-	return datepicker.regional.ru;
+		datepicker.regional.ru = {
+			closeText: "Закрыть",
+			prevText: "&#x3C;Пред",
+			nextText: "След&#x3E;",
+			currentText: "Сегодня",
+			monthNames: [ "Январь","Февраль","Март","Апрель","Май","Июнь",
+			"Июль","Август","Сентябрь","Октябрь","Ноябрь","Декабрь" ],
+			monthNamesShort: [ "Янв","Фев","Мар","Апр","Май","Июн",
+			"Июл","Авг","Сен","Окт","Ноя","Дек" ],
+			dayNames: [ "воскресенье","понедельник","вторник","среда","четверг","пятница","суббота" ],
+			dayNamesShort: [ "вск","пнд","втр","срд","чтв","птн","сбт" ],
+			dayNamesMin: [ "Вс","Пн","Вт","Ср","Чт","Пт","Сб" ],
+			weekHeader: "Нед",
+			dateFormat: "dd.mm.yy",
+			firstDay: 1,
+			isRTL: false,
+			showMonthAfterYear: false,
+			yearSuffix: "" };
+		datepicker.setDefaults( datepicker.regional.ru );
 
-	} ) );
+		return datepicker.regional.ru;
+
+		} ) );
+	};
 
 	// Поля в корзине
 	function inputToggleActive() {
@@ -341,7 +342,6 @@ $(document).ready(function () {
 					'overflow-y': 'visible'
 				});
 			}
-			console.log(itemLength);
 		}		
 	}
 	cartScroll();
